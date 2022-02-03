9c7346a80d2d25bb90ea70f8f4b68f6476004fcb
Rodrigo Pereyra <pereyrarodrigo@hotmail.com>
2022-02-03 04:18:12 -0300

    horizontal slider done, vertical WIP

---
+++ js/sliderVertical.js
@@ -0,0 +1,53 @@
+class Slider {
+    constructor(options) {
+        this.sections = document.querySelectorAll(options.section);
+        this.navigation = document.querySelector(options.dots);
+
+        this.navigation.addEventListener('click', this.scrollToSection.bind(this));
+        window.addEventListener('scroll', this.setDotStatus.bind(this));
+    }
+
+    removeDotStyles() {
+        const dots = this.navigation;
+        const is_active = dots.querySelector('.is-active');
+
+        if (is_active != null) {
+            is_active.classList.remove('is-active');
+        }
+    }
+
+    setDotStatus() {
+        const scroll_position = window.scrollY;
+        const dots = Array.from(this.navigation.children);
+
+        this.sections.forEach((section, index) => {
+            const half_window = window.innerHeight / 2;
+            const section_top = section.offsetTop;
+
+            if (scroll_position > section_top - half_window && scroll_position < section_top + half_window) {
+                this.removeDotStyles();
+                dots[index].classList.add('is-active');
+            }
+        })
+    }
+
+    scrollToSection(e) {
+        const dots = Array.from(this.navigation.children);
+        const window_height = window.innerHeight;
+
+        dots.forEach((dot, index) => {
+            if (dot == e.target) {
+
+                window.scrollTo({
+                    top: window_height - window_height * index,
+                    behavior: 'smooth',
+                });
+            }
+        });
+    }
+}
+
+new Slider({
+    section: '.section',
+    dots: '#js-dots',
+});
