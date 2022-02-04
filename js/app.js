770dc9bd75957f5df4af1438aac977d42c385e0e
nicojoaquin <nicojoaquin1998@gmail.com>
2022-02-04 13:43:07 -0300

    'pushbar'

--- js/app.js
+++ js/app.js
@@ -1,30 +1,36 @@
-const inicio = document.querySelector('#inicio');
-const intro = document.querySelector('#intro');
+//Variables
+const btnInicio = document.querySelector('#inicio');
+const btnIntro = document.querySelector('#intro');
 const firstPage = document.querySelector('#first-page');
 const secondPage = document.querySelector('#second-page');
 
+//Funciones
+
+//Al clickear en "Iniciar" se oculta la primera página y aparece la segunda
 const goNextPage = e => {
   if(e.target.classList.contains('btn-iniciar')) {
     firstPage.classList.add('d-none');
     secondPage.classList.remove('d-none');
-    intro.classList.remove('d-none');
+    btnIntro.classList.remove('d-none');
     setTimeout(() => {
-      inicio.classList.add('d-none')
+      btnInicio.classList.add('d-none')
     }, [1200]);
   };
 };
 
+//Al clickear en la flechita de "Introducción" se oculta la segunda página y aparece la primera
 const goPreviousPage = e => {
   if(e.target.classList.contains('flecha')) {
     secondPage.classList.add('d-none');
-    inicio.classList.remove('d-none');
+    btnInicio.classList.remove('d-none');
     setTimeout(() => {
-      intro.classList.add('d-none');
+      btnIntro.classList.add('d-none');
       firstPage.classList.remove('d-none');
     }, [1200]);
   };
 };
 
-inicio.addEventListener('click', goNextPage);
-intro.addEventListener('click', goPreviousPage);
+//Listeners
+btnInicio.addEventListener('click', goNextPage);
+btnIntro.addEventListener('click', goPreviousPage);
 
--- js/pushbar.js
+++ js/pushbar.js
@@ -1,90 +1,8 @@
-class Pushbar {
-    constructor(config = { overlay: true, blur: false }) {
-        this.activeId;
-        this.activeElement;
-        this.overlayElement;
-        if (config.overlay) {
-            this.overlayElement = document.createElement('div');
-            this.overlayElement.classList.add('pushbar_overlay');
-            document.querySelector('body').appendChild(this.overlayElement);
-        }
-        if (config.blur) {
-            const mainContent = document.querySelector('.pushbar_main_content');
-            if (mainContent) {
-                mainContent.classList.add('pushbar_blur');
-            }
-        }
-        this.bindEvents();
-    }
+//Pushbar config
 
-    emitOpening() {
-        const event = new CustomEvent('pushbar_opening', { bubbles: true, detail: { element: this.activeElement, id: this.activeId } });
-        this.activeElement.dispatchEvent(event);
-    }
-
-    emitClosing() {
-        const event = new CustomEvent('pushbar_closing', { bubbles: true, detail: { element: this.activeElement, id: this.activeId } });
-        this.activeElement.dispatchEvent(event);
-    }
-
-    handleOpenEvent(e) {
-        e.preventDefault();
-        const pushbarId = e.currentTarget.getAttribute('data-pushbar-target');
-        this.open(pushbarId);
-    }
-
-    handleCloseEvent(e) {
-        e.preventDefault();
-        this.close();
-    }
-
-    handleKeyEvent(e) {
-        if (e.keyCode === 27) this.close();
-    }
-
-    bindEvents() {
-        const triggers = document.querySelectorAll('[data-pushbar-target]');
-        const closers = document.querySelectorAll('[data-pushbar-close]');
-        triggers.forEach(trigger => trigger.addEventListener('click', e => this.handleOpenEvent(e), false));
-        closers.forEach(closer => closer.addEventListener('click', e => this.handleCloseEvent(e), false));
-        if (this.overlayElement) {
-            this.overlayElement.addEventListener('click', e => this.handleCloseEvent(e), false);
-        }
-        document.addEventListener('keyup', e => this.handleKeyEvent(e));
-    }
-
-    open(pushbarId) {
-        if (this.activeId === String(pushbarId) || !pushbarId) return;
-        if (this.activeId && this.activeId !== String(pushbarId)) this.close();
-        this.activeId = pushbarId
-        this.activeElement = document.querySelector(`[data-pushbar-id="${this.activeId}"]`)
-        if (!this.activeElement) return;
-        this.emitOpening();
-        this.activeElement.classList.add('opened');
-        const pageRootElement = document.querySelector('html')
-        pageRootElement.classList.add('pushbar_locked');
-        pageRootElement.setAttribute('pushbar', pushbarId)
-    }
-
-    close() {
-        if (!this.activeId) return;
-        this.emitClosing();
-        this.activeElement.classList.remove('opened');
-        const pageRootElement = document.querySelector('html')
-        pageRootElement.classList.remove('pushbar_locked');
-        pageRootElement.removeAttribute('pushbar')
-        this.activeId = null;
-        this.activeElement = null;
-    }
-}
-
-//Config personalizada
+//Variables
 const buttonContainer = document.querySelector("#btn-pushbar");
-
-let pushbar = new Pushbar({
-  blur: true,
-  overlay: false,
-});
+const pushbar = document.querySelector('#pushbar-menu')
 
 //Función que cambia el texto del boton
 const changeButton = (className, text) => {
@@ -93,13 +11,14 @@ const changeButton = (className, text) => {
     `;
 };
 
+//Listener
 //Si dice "ver mas", lo abre, y si dice "ver menos", lo cierra, luego cambia el texto
 buttonContainer.addEventListener("click", ({target}) => {
   if (target.classList.contains("buttonOne")) {
-    pushbar.open("pushbar-menu");
+    pushbar.classList.add('pushbar-open');
     changeButton("buttonTwo", "Ver menos");
   } else if (target.classList.contains("buttonTwo")) {
-    pushbar.close();
+    pushbar.classList.remove('pushbar-open')
     changeButton("buttonOne", "Ver mas");
   }
 });
